83e0e85b85daa88ebab525dbd736604818ecd33a
Jérôme Respaut <jerome.respaut@fr.clara.net>
2020-11-19 14:13:50 +0100

    AZ-344/AZ-273: Keep retrop compat with TF 0.12/AzureRM provider

--- versions.tf
+++ versions.tf
@@ -1,10 +1,9 @@
-
 terraform {
-  required_version = ">= 0.13"
+  required_version = "> 0.12.26"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 2.0"
+      version = ">= 1.44"
     }
   }
 }
