d0609f20159b8f32323dee56e9b6cce025553141
Jérôme Respaut <jerome.respaut@fr.clara.net>
2022-09-19 11:54:26 +0200

    GH-3: Use network watcher data source only if flow logs are enabled

--- r-flow-log.tf
+++ r-flow-log.tf
@@ -1,4 +1,6 @@
 data "azurerm_network_watcher" "nw" {
+  for_each = toset(var.flow_log_enabled ? ["enabled"] : [])
+
   name                = var.network_watcher_name
   resource_group_name = var.network_watcher_resource_group_name
 }
@@ -7,14 +9,14 @@ resource "azurerm_network_watcher_flow_log" "nwfl" {
   count = var.flow_log_enabled ? 1 : 0
 
   name                 = local.flow_log_name
-  network_watcher_name = data.azurerm_network_watcher.nw.name
-  resource_group_name  = data.azurerm_network_watcher.nw.resource_group_name
+  network_watcher_name = data.azurerm_network_watcher.nw["enabled"].name
+  resource_group_name  = data.azurerm_network_watcher.nw["enabled"].resource_group_name
 
   network_security_group_id = azurerm_network_security_group.nsg.id
   storage_account_id        = var.flow_log_storage_account_id
   enabled                   = var.flow_log_logging_enabled
 
-  location = coalesce(var.flow_log_location, data.azurerm_network_watcher.nw.location)
+  location = coalesce(var.flow_log_location, data.azurerm_network_watcher.nw["enabled"].location)
 
   retention_policy {
     enabled = var.flow_log_retention_policy_enabled
