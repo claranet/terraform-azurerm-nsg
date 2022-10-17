7c4244bc5f61163c7357e3d87ec72a8b87c988bf
Jérôme Respaut <jerome.respaut@fr.clara.net>
2022-10-17 12:00:02 +0200

    GH-5: Fixes output, backport updates, README

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "network_security_group_id" {
-  description = "Network security group id"
+  description = "Network security group ID"
   value       = azurerm_network_security_group.nsg.id
 }
 
@@ -9,6 +9,6 @@ output "network_security_group_name" {
 }
 
 output "network_watcher_flow_log_id" {
-  description = "Network watcher flow log id"
+  description = "Network watcher flow log ID"
   value       = var.flow_log_enabled ? azurerm_network_watcher_flow_log.nwfl[0].id : null
 }
